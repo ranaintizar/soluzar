51db5bb98a5c77941cd277d353c240272a269b1e
ranaintizar <intizaralirana2@gmail.com>
2023-06-26 00:48:31 +0500

    Create Hero Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // import Button from "components/button";
-import FeatureCard from "components/feature-card";
+// import FeatureCard from "components/feature-card";
 import Header from "components/header";
+import HeroSection from "components/hero-section";
 // import Intro from "components/intro-section";
 import Head from "next/head";
 
@@ -18,7 +19,8 @@ export default function Home() {
         {/* <Intro /> */}
         {/* <Button /> */}
         {/* <h1>Soluzar</h1> */}
-        <FeatureCard />
+        {/* <FeatureCard /> */}
+        <HeroSection />
       </main>
     </>
   );
---
+++ src/components/hero-section/HeroSection.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+import clsx from "clsx";
+
+import Button from "components/button";
+
+import Img from "assets/image-2.png";
+
+import stl from "./HeroSection.module.scss";
+
+interface Props {
+  swap: Boolean;
+  src: string;
+  heading: string;
+  headingSpecial: string;
+  desc: string;
+}
+
+const HeroSection = ({ swap, src, heading, headingSpecial, desc }: Props) => {
+  return (
+    <div className={clsx(stl.heroSection, swap ? stl.right : "")}>
+      <div className={stl.img}>
+        <Image src={src} alt="image" layout="responsive" />
+      </div>
+      <div className={stl.content}>
+        <div className={stl.heading}>
+          {heading} <span>{headingSpecial}</span>
+        </div>
+        <span className={stl.desc}>{desc}</span>
+        <div className={stl.btn}>
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+HeroSection.defaultProps = {
+  swap: false,
+  src: Img,
+  heading: "We have been improving our product",
+  headingSpecial: "for many years",
+  desc: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized",
+};
+
+export default HeroSection;
